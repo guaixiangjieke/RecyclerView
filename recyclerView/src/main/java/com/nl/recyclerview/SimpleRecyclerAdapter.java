33fee6c20b2277e23c30cbadb07bb474c86f4a3e
NiuLei <guaixiangjieke@126.com>
2018-10-10 10:44:26 +0800

    1、添加simpleHolder 构造函数

--- recyclerView/src/main/java/com/nl/recyclerview/SimpleRecyclerAdapter.java
+++ recyclerView/src/main/java/com/nl/recyclerview/SimpleRecyclerAdapter.java
@@ -11,4 +11,8 @@ public class SimpleRecyclerAdapter<T> extends RecyclerAdapter<T> {
     public SimpleRecyclerAdapter(List<T> data) {
         super(RecyclerViewHolder.class, android.R.layout.simple_list_item_1, data);
     }
+
+    public SimpleRecyclerAdapter(int layoutId, List<T> data) {
+        super(RecyclerViewHolder.class, layoutId, data);
+    }
 }
